73ece0ac507171bc2654b9d62b75a1e5056459e3
pramasam@g.syr.edu <pramasam@g.syr.edu>
2025-04-16 04:45:05 +0000

    fact job error fixed

--- models/marts/employer_hiring/fact_job_application.sql
+++ models/marts/employer_hiring/fact_job_application.sql
@@ -2,36 +2,33 @@
 
 WITH job_applications AS (
     SELECT
-        e.job_id, 
-        e.student_id,  
-        e.application_status,  
-        e.application_date,
-        e.interview_date, 
-        e.hiring_status, 
-        e.job_role,  
-        e.company_name, 
-        e.salary_offered,
-        e.application_channel, 
-        e.region, 
-        COUNT(e.student_id) AS total_applications  
-    FROM {{ ref('stg_employer_hiring') }} AS e
+        job_id,
+        application_status,
+        application_date,
+        interview_date,
+        hiring_status,
+        job_role,
+        company_name,
+        salary_offered,
+        application_channel,
+        region,
+        COUNT(*) AS total_applications
+    FROM {{ ref('stg_employer_hiring') }}
     GROUP BY
-        e.job_id,
-        e.student_id,
-        e.application_status,
-        e.application_date,
-        e.interview_date,
-        e.hiring_status,
-        e.job_role,
-        e.company_name,
-        e.salary_offered,
-        e.application_channel,
-        e.region
+        job_id,
+        application_status,
+        application_date,
+        interview_date,
+        hiring_status,
+        job_role,
+        company_name,
+        salary_offered,
+        application_channel,
+        region
 )
 
 SELECT
     job_id,
-    student_id,
     application_status,
     application_date,
     interview_date,
