d090a93cd2e5865e156884e7a8cc4ad541196c39
pramasam@g.syr.edu <pramasam@g.syr.edu>
2025-04-16 00:57:50 +0000

    made changes in staging

--- models/staging/stg_employer_hiring.sql
+++ models/staging/stg_employer_hiring.sql
@@ -1,12 +1,1 @@
-SELECT
-  Job_ID,
-  Company_Name,
-  Job_Title,
-  Required_Skills,
-  Salary_Offered,
-  Job_Location,
-  TO_DATE(Job_Posting_Date, 'DD-MM-YYYY') AS Job_Posting_Date,
-  Applications_Received,
-  Hires_Made,
-  Employment_Type
-FROM skillsync_raw.staging.employer_hiring;
+SELECT * FROM skillsync_raw.staging.employer_hiring;
